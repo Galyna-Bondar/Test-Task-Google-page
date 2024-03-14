6bad2b90b72d996159e2809caeac51fe72268526
Galyna Bondar <galyna-bondar@i.ua>
2024-03-14 20:55:30 +0200

    first commit

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//З використанням синтаксису jQuery:
+var form = $('#form');
+form.on('submit', function (e) {
+  e.preventDefault();
+});
+$(document).ready(function () {
+  var changingLogoBtn = $('#btn_changing_logo');
+  changingLogoBtn.on('click', function (event) {
+    event.preventDefault();
+    var logo = $('#logo');
+    var logoSrc = logo.attr('src');
+
+    if (logoSrc === '/assets/images/google_logo.png') {
+      logo.attr('src', '/assets/images/odditytech_logo.png');
+    } else {
+      logo.attr('src', '/assets/images/google_logo.png');
+    }
+  });
+}); // Те саме на Стандартному JavaScript
+// const form = document.getElementById('form'); 
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault(); 
+// });
+// document.addEventListener('DOMContentLoaded', function() {
+//     let changingLogoBtn = document.getElementById('btn_changing_logo');
+//     changingLogoBtn.addEventListener('click', function(event) {
+//         event.preventDefault(); 
+//         let logo = document.getElementById('logo');
+//         let logoSrc = logo.getAttribute('src');
+//         if (logoSrc === '/assets/images/google_logo.png') {
+//             logo.setAttribute('src', '/assets/images/odditytech_logo.png');
+//         } else {
+//             logo.setAttribute('src', '/assets/images/google_logo.png');
+//         }
+//     });
+// });
---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+//З використанням синтаксису jQuery:
+const form = $('#form');
+form.on('submit', function(e) {
+    e.preventDefault();
+});
+
+$(document).ready(function() {
+    let changingLogoBtn = $('#btn_changing_logo');
+    changingLogoBtn.on('click', function(event) {
+        event.preventDefault(); 
+        
+        let logo = $('#logo');
+        
+        let logoSrc = logo.attr('src');
+        if (logoSrc === '/assets/images/google_logo.png') {
+            logo.attr('src', '/assets/images/odditytech_logo.png');
+        } else {
+            logo.attr('src', '/assets/images/google_logo.png');
+        }
+    });
+});
+
+
+// Те саме на Стандартному JavaScript
+// const form = document.getElementById('form'); 
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault(); 
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     let changingLogoBtn = document.getElementById('btn_changing_logo');
+//     changingLogoBtn.addEventListener('click', function(event) {
+//         event.preventDefault(); 
+        
+//         let logo = document.getElementById('logo');
+        
+//         let logoSrc = logo.getAttribute('src');
+//         if (logoSrc === '/assets/images/google_logo.png') {
+//             logo.setAttribute('src', '/assets/images/odditytech_logo.png');
+//         } else {
+//             logo.setAttribute('src', '/assets/images/google_logo.png');
+//         }
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
